2fd6ba51ce54961bd1d0c7abc819db473b4bcc22
Hoang Dinh Bang <hoangbang2941994@gmail.com>
2022-03-23 21:54:47 +0700

    update router v.9

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,40 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//admin______________________________________________
+//Route::prefix(['prefix' => 'admin','middleware' => ['auth', 'admin']])->group(function () {
+//    Route::prefix('categories')->group(function () {
+//        Route::get('/', [
+//            'as' => 'categories.index',
+//            'uses' => 'CategoryController@index',
+//            'middleware' => 'can:category-list'
+//
+//        ]);
+//    });
+//});
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('/dashboard', [
+        'as' => 'dashboard',
+        'uses' => [DashboardController::class, 'login'],
+    ]);
+});
+
+
+Route::middleware(['auth', 'user'])->group(function () {
+    Route::get('/dashboard', [
+        'as' => 'dashboard',
+        'uses' => [DashboardController::class, 'login'],
+    ]);
+});
+
+
+
+//Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'middleware' => ['auth', 'admin']], function () {
+//    Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+//});
+//
+////user_______________________________________________
+//Route::group(['prefix' => 'user', 'as' => 'user.', 'namespace' => 'User', 'middleware' => ['auth', 'user']], function () {
+//    Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+//});
