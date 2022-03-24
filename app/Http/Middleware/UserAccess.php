a6412da5327c3b80b873e43a2f128947656fa213
Hoang Dinh Bang <hoangbang2941994@gmail.com>
2022-03-24 23:17:07 +0700

    fix middleware adminaccess and useraccess

--- app/Http/Middleware/UserAccess.php
+++ app/Http/Middleware/UserAccess.php
@@ -4,6 +4,13 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+//Illuminate\Auth\AuthManager
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+
 
 class UserAccess
 {
@@ -16,7 +23,9 @@ class UserAccess
      */
     public function handle(Request $request, Closure $next)
     {
-        if (Auth::check()&& Auth::user()->role->id == 2)
+//        dd(Auth()::user());
+//        dd(Auth::user()->role->id == 2);
+        if (Auth::check() && Auth::user()->role->id == 2)
         {
             return $next($request);
         }else
--- routes/web.php
+++ routes/web.php
@@ -22,30 +22,31 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 //admin______________________________________________
-//Route::prefix(['prefix' => 'admin','middleware' => ['auth', 'admin']])->group(function () {
-//    Route::prefix('categories')->group(function () {
-//        Route::get('/', [
-//            'as' => 'categories.index',
-//            'uses' => 'CategoryController@index',
-//            'middleware' => 'can:category-list'
-//
-//        ]);
-//    });
+Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'middleware' => ['auth', 'admin']], function () {
+
+    Route::get('/dashboard',[App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('dashboard');
+});
+Route::group(['prefix' => 'user', 'as' => 'user.', 'namespace' => 'User', 'middleware' => ['auth', 'user']], function () {
+
+    Route::get('/dashboard',[App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('dashboard');
+
+});
+
+
+//Route::middleware(['auth', 'admin'])->group(function () {
+//    Route::get('/dashboard', [
+//        'as' => 'admin.dashboard',
+//        'uses' => [DashboardController::class, 'login'],
+//    ]);
 //});
-Route::middleware(['auth', 'admin'])->group(function () {
-    Route::get('/dashboard', [
-        'as' => 'dashboard',
-        'uses' => [DashboardController::class, 'login'],
-    ]);
-});
-
-
-Route::middleware(['auth', 'user'])->group(function () {
-    Route::get('/dashboard', [
-        'as' => 'dashboard',
-        'uses' => [DashboardController::class, 'login'],
-    ]);
-});
+//
+//
+//Route::middleware(['auth', 'user'])->group(function () {
+//    Route::get('/dashboard', [
+//        'as' => 'user.dashboard',
+//        'uses' => [DashboardController::class, 'login'],
+//    ]);
+//});
 
 
 
